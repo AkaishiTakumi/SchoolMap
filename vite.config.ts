b6d3cae41de11b650b99ac4c0e73c921f1a69dfd
AkaisiTakumi <115193708+AkaisiTakumi@users.noreply.github.com>
2025-02-12 19:42:48 +0900

    Squashed commit of the following:
    commit 4bcc2d749ef2bcb71324f0b829e468ceab703ec8
    Author: AkaisiTakumi <115193708+AkaisiTakumi@users.noreply.github.com>
    Date:   Wed Feb 12 19:42:27 2025 +0900
    
        feat: gh-pagesパッケージを追加
    
        githubにデプロイする #2
        package.jsonにgh-pagesの依存関係を追加しました。
    
    commit 3c5f003f36f048f2560e65fc822680b827ad8936
    Author: AkaisiTakumi <115193708+AkaisiTakumi@users.noreply.github.com>
    Date:   Wed Feb 12 19:41:14 2025 +0900
    
        feat: デプロイ用スクリプトとホームページURLを追加
    
        githubにデプロイする #2
        - package.jsonにホームページURLを追加
        - デプロイ用のスクリプト（predeploy, deploy）を追加
        - vite.config.tsにGitHubリポジトリ名を設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SchoolMap/', // GitHubリポジトリ名を設定ins: [react()],
   plugins: [react()],
 })
